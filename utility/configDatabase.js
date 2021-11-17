82c133ab9356a94970646d5eebd0eb1b8008d48d
dadarvin <darvin@ui.ac.id>
2021-11-17 22:53:43 +0700

    configDatabase change 2

--- utility/configDatabase.js
+++ utility/configDatabase.js
@@ -1,7 +1,7 @@
 const DATABASE_URL = "ec2-34-198-189-252.compute-1.amazonaws.com";
 
 const { Pool } = require('pg');
-const pool = new Pool({
+const client = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false

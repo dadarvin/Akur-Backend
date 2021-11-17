a0b8738ff615b98b90809d3b54d9aa22b0df0390
dadarvin <darvin@ui.ac.id>
2021-11-17 21:38:09 +0700

    connect to postgres change

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null };
-      res.render('pages/db', results);
+      res.json(results);
       client.release();
     } catch (err) {
       console.error(err);

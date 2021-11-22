ca2291f19a1e15b6b8561aa9ec811e5a5aa8605b
dadarvin <darvin@ui.ac.id>
2021-11-22 16:20:07 +0700

    change format hist

--- controllers/menu/account.js
+++ controllers/menu/account.js
@@ -169,10 +169,17 @@ app.post("/history", async (req, res) => {
     try {
         const user_id = req.body.user_id;
 
-        // const values = await client.query(`SELECT q.id_qr, q.nama_kurir, q.no_resi, a.jenis_kurir, q.date FROM qr_scan AS q LEFT JOIN api_info as a on q.id_qr = a.id_qr WHERE user_id = ${user_id}`);
-        const values = await client.query(`SELECT * FROM qr_scan WHERE user_id = ${user_id}`);
+        const values = await client.query(`SELECT q.id_qr, q.nama_kurir, q.no_resi, a.jenis_kurir, q.date FROM qr_scan AS q LEFT JOIN api_info as a on q.id_qr = a.id_qr WHERE user_id = ${user_id}`);
+        // const values = await client.query(`SELECT * FROM qr_scan WHERE user_id = ${user_id}`);
 
-        res.json(values.rows);
+        // res.json(values.rows);
+        res.json({
+            id_qr: values.rows[0].id_qr,
+            nama_kurir: values.rows[0].nama_kurir,
+            no_resi: values.rows[0].no_resi,
+            jenis_kurir: values.rows[0].jenis_kurir,
+            date: values.rows[0].date,
+        });
     } catch (err) {
         console.error(err.message);
         res.send(false);
@@ -186,6 +193,7 @@ app.post("/apiInfo", async (req, res) => {
         const barcodeInfo = await client.query(`SELECT nama_kurir, no_resi FROM qr_scan WHERE id_qr = ${id_qr}`);
 
         res.json(values.rows);
+
     } catch (err) {
         console.error(err.message);
         res.send(false);
